4727a01a790f296b7e5a8a524135d0c0f92c2c17
yash tiwari <yashtiwari700714@gmail.com>
2025-05-20 00:33:50 +0530

    clerk middleware

---
+++ middleware.ts
@@ -0,0 +1,32 @@
+import {
+  clerkMiddleware,
+  createRouteMatcher,
+  auth,
+} from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isPublicRoute = createRouteMatcher(["/", "/signin(.*)", "/signup(.*)"]);
+
+export default clerkMiddleware(async (auth, request) => {
+  const user = auth();
+  const userId = (await user).userId;
+  const url = new URL(request.url);
+
+  if (userId && isPublicRoute(request) && url.pathname !== "/") {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  // Protect non-public routes
+  if (!isPublicRoute(request)) {
+    await auth.protect();
+  }
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
